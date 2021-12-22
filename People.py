4b938f1fbc0619bfe3fa68a312d7f7cce85011c5
singhmandeep06@gmail.com <singhmandeep06gmail.com@singhmandeep06gmailcoms-MacBook-Air.local>
2021-12-21 19:25:58 -0500

    Second commit

--- People.py
+++ People.py
@@ -1,22 +1,24 @@
 import json
+from json.decoder import JSONDecodeError
 
 
 class People:
 
     
-    def __init__(self,first_name,last_name,address,phone_number,account_bal=0):
+    def __init__(self,first_name,last_name,address,phone_number,account_bal=0,):
        self.first_name = first_name
        self.last_name = last_name
        self.address = address
        self.phone_number = phone_number
        self.account_bal = account_bal
        
+       
        self.rec_json ={
-                'f_name': self.first_name,
+                'first_name': self.first_name,
                 'l_name': self.last_name,
                 'adr': self.address,
                 'ph_number': self.phone_number,
-                'Cash_in_account': self.account_bal
+                'Cash_in_account': self.account_bal,
        }
 
         
@@ -34,47 +36,118 @@ class People:
         with open('record.json','w') as file:
             json.dump(data,file,indent=4)
 
-class Employe(People):
+    def append_json(self,new_data):
 
-    def __init__(self,first_name,last_name,address,phone_number):
-        super().__init__(first_name, last_name, address, phone_number)
-        pass
-        
+        with open("record.json", "r+") as file:
+            data = json.load(file)
+            data.update(new_data)
+            file.seek(0)
+            json.dump(data, file,indent=4)
 
+    
 
 class Account(People):
   
 
-    def __init__(self,account_bal,checking_acc,saving_rate,fees_on_wid,deposit_amount):
-        super().__init__(first_name,last_name,address,phone_number,account_bal)
-
-        self.checking_acc = checking_acc 
-        self.savings_rate = saving_rate
-        self.fees_on_wid = fees_on_wid
+    def __init__(self,account_bal,deposit_amount):
+        super().append_json
+        super().update_json
         self.deposit_amount = deposit_amount
+        self.account_bal =account_bal
 
-    def savings(self):
-        pass 
+
+    def show_balance(self):
+        data  = open('record.json')
+        bio_data= json.load(data)
+        data.close()
+        balance = bio_data['Cash_in_account']
+
+        print(f'Your Account Balance is :${balance}')
+
+        
+
 
     def deposit(self,deposit_amount):
-        
+     
         self.account_bal = self.account_bal + self.deposit_amount
+        balance  = {'Cash_in_account': self.account_bal}
 
-
-        with open('record.json','r') as file:
-            data = json.load(file)
-            data['Cash_in_account'] = self.account_bal 
+        Account.append_json(self,balance)
+        Account.show_balance(self)
         
-        with open('record.json','w') as file:
-            json.dump(data,file,indent=4)
 
-
-
+    def withdraw(self,amount):
+        self.amount = amount
+        json_file = open("record.json")
+        variables = json.load(json_file)
+        json_file.close()
+        variables['Cash_in_account']= variables['Cash_in_account'] - self.amount
 
         
-    #def first_deposit(self,cash_deposit,account_bal):
+        Account.update_json(self,variables)
+        Account.show_balance(self)
 
-        #account_bal = account_bal+cash_deposit
+
+    def account_summry(self):
+        summry = open('record.json')
+        data = json.load(summry)
+        summry.close()
+        print('++++++++++++++++++Account Summry++++++++++++++++++++++++')
+        print(json.dumps(data, sort_keys=False, indent=4))
+
+class Services(People):
+    
+    def __init__(self,credit_score):
+        super().append_json
+        self.credit_score = credit_score
+
+    def monthly_payment(self,credit_score):
+        
+        def monthly_instalments(P,rate,months):
+            payment = (rate/12) * (1/(1-(1+rate/12)**(-months)))*P
+            return round(payment,2)
+
+
+
+        if credit_score in  range(0,550):
+            P = 5000
+            rate = 0.12
+            months = 60
+            info = {"P":5000,
+            'rate':'12%',
+            'Months':60}
+            
+            Services.append_json(self,info)
+
+            print(f"\nYou are aproved for line of credit :{P} \nInterest rate: {rate}\nNumber of Months :{months}\nmonthly payments :")
+            print(monthly_instalments(P,rate,months))
+
+
+        elif credit_score in range(551,650):
+            P = 10000
+            rate = 0.08
+            months = 60
+            info = {"P":1000,
+            'rate':'8%',
+            'Months':60}
+            Services.append_json(self,info)
+
+            print(f"\nYou are aproved for line of credit :{P} \nInterest rate: {rate}\nNumber of Months :{months}\nmonthly payments :")
+            print(monthly_instalments(P,rate,months))
+
+        else :
+            P = 15000
+            rate = 0.04
+            months = 60
+            info = {"P":15000,
+            'rate':"4%",
+            'Months':60}
+            Services.append_json(self,info)
+
+            print(f"\nYou are aproved for line of credit :{P} \nInterest rate: {rate}\nNumber of Months :{months}\nmonthly payments :")
+            print(monthly_instalments(P,rate,months))
+
+
 
 
 
@@ -83,36 +156,104 @@ if __name__ == '__main__':
     print("\n****************WELCOME TO PEOPLE'S BANK***********************\n")
     first_name = input('what is your first name: ')
     last_name =input('what is last name: ')
-    address = input('Plsese enter tour address: ')
+    address = input('Plsese enter your address: ')
     phone_number = int(input('Please enter your 10 digit phone number: '))
     bank_obj = People(first_name,last_name,address,phone_number)
     bank_obj.data_rec_json()
-    if_emp_or_na = int(input("\nPress 1 if you are employee of People's Bank \n Press 2 if you are Customer: "  ))
+    if_emp_or_na = int(input("\nPress 1 if you are employee of People's Bank \nPress 2 if you are Customer: "  ))
     
     if if_emp_or_na == 1:
         print(f"\n++++++++++++++WELCOME TO PEOPLE'S BANK FAMILY {first_name} {last_name}++++++++++++++++++++\n")
         emp = People(first_name,last_name,address,phone_number,account_bal=0)
         sal_empl = { 'Base_salary': 60000 }
         emp.update_json(sal_empl)
+        print('your base salary is ${Base_salary}'.format(**sal_empl))
+        print(f'\nWould Like to make Deposit {first_name} {last_name} ?\n')
+        asking_4_deposit = int(input('\nPlease Enter 1 for "Yes" \nOr 2 for "NO": '))
+
+
+        if asking_4_deposit == 1:
+            account_bal = 0
+            deposit_amount = int(input('Please Enter the ammount: '))
+            bank_account = Account(account_bal,deposit_amount)
+            bank_account.deposit(deposit_amount)
+
+        if asking_4_deposit == 2:
+            summry = Account(account_bal,deposit_amount)
+            summry.account_summry()
+           
+
 
     if if_emp_or_na == 2:
         print(f"\n********************Thank You for being a People's Bank Customer {first_name} {last_name}***********************\n")
-        Account_type = int(input("\n Press 1 to open Checking Account \n Press 2 to open a Savings  Account:  "))
+        Account_type = int(input("\nPress 1 to open Checking Account \nPress 2 to open a Savings  Account:  "))
 
         if Account_type == 1:
             account = {'Account_type': "Checking Account"}
             bank_obj.update_json(account)
             print(f'\nWould Like to make Deposit {first_name} {last_name} ?\n')
-            asking_4_deposit = int(input('Please Enter 1 for "Yes" \n Or 2 for "NO": \n '))
+            asking_4_deposit = int(input('\nPlease Enter 1 for "Yes" \nOr 2 for "NO": '))
+
 
             if asking_4_deposit == 1:
-                deposit_amount = int(input('Please Enter the ammount'))
-                bank_account = Account(account_bal,checking_acc,saving_rate,fees_on_wid)
+                account_bal = 0
+                deposit_amount = int(input('Please Enter the ammount: '))
+                bank_account = Account(account_bal,deposit_amount)
                 bank_account.deposit(deposit_amount)
-               
                 
+            if asking_4_deposit ==2:
+               
+                summry = Account(account_bal,deposit_amount)
+                summry.account_summry()
+
+        if Account_type == 2:
+            account = {'Account_type': "Saveings Account"}
+            rate = {'savings_intrest_rate':0.03}
+            bank_obj.update_json(account)
+            bank_obj.update_json(rate)
+            print('\nOur Bank Provide Very Compatative rate of:{savings_intrest_rate}%\n'.format(**rate))
+            print(f'\nWould Like to make Deposit {first_name} {last_name} ?\n')
+            asking_4_deposit = int(input('\nPlease Enter 1 for "Yes" \nOr 2 for "NO": '))
+
+            if asking_4_deposit == 1:
+                account_bal = 0
+                deposit_amount = int(input('Please Enter the ammount: '))
+                bank_account = Account(account_bal,deposit_amount)
+                bank_account.deposit(deposit_amount)
+
+            if asking_4_deposit == 2:
+                summry = Account(account_bal,deposit_amount)
+                summry.account_summry()
+
+    print(f'\nDo you need a line of credit {first_name} {last_name} ?\n')
+    l_credit = int(input('Please Enter 1 for "Yes" or Enter 2 For "NO" :'))
+
+    if l_credit == 1:
+        credit_score = int(input(f'What is your Credit Score {first_name} {last_name} ?'))
+        credit_line = Services(credit_score)
+        credit_line.monthly_payment(credit_score)
+
+    if l_credit ==2:
+        
+        summry = Account(account_bal,deposit_amount)
+        summry.account_summry()
 
 
+    print(f'\nWould You like withdraw some money {first_name} {last_name}?')
+    withdraw_amt = (int(input('\nPress 1 For "Yess" or \nPress 2 for "NO" :')))
+
+    if withdraw_amt == 1:
+        amount = int(input('Plase enter a amount that you would like to withdraw: '))
+        withdraw = Account(account_bal,deposit_amount)
+        withdraw.withdraw(amount)
+    
+    if withdraw_amt ==2:
+        
+        summry = Account(account_bal,deposit_amount)
+        summry.account_summry()
+
+    summry = Account(account_bal,deposit_amount)
+    summry.account_summry()
 
 
-
+  
--- hold_on.py
+++ hold_on.py
@@ -1,72 +1,47 @@
 import json
 
 
+import json
+
+
 class People:
 
     
-    def __init__(self,first_name,last_name,address,phone_number):
-       self.firstname = first_name
+    def __init__(self,first_name,last_name,address,phone_number,account_bal=0,account_pin):
+       self.first_name = first_name
        self.last_name = last_name
        self.address = address
        self.phone_number = phone_number
+       self.account_bal = account_bal
+       self.account_pin = account_pin
        
-    def data_rec_json(self):
-        
-        rec_json = {
+       self.rec_json ={
                 'f_name': self.first_name,
                 'l_name': self.last_name,
                 'adr': self.address,
                 'ph_number': self.phone_number,
-                'Cash_in_account': self.account_bal
-                }
+                'Cash_in_account': self.account_bal,
+                'account_pin': self.account_pin
+       }
+
         
+    def data_rec_json(self):
+        
+        with open('record.json','w') as file:
+            json.dump(self.rec_json,file,indent=4)
 
-        with open('record.json','w') as info:
-            json.dump(rec_json,info,indent=4)
-            
-class Acount(People):
+    def update_json(self,info):
+        self.info = info
+        with open('record.json','r') as file:
+            data = json.load(file)
+            data.update(self.info)
+        
+        with open('record.json','w') as file:
+            json.dump(data,file,indent=4)
 
-    def __init__(self, first_name, last_name, address, phone_number,checking_account,savings_account):
+class Employe(People):
+
+    def __init__(self,first_name,last_name,address,phone_number):
         super().__init__(first_name, last_name, address, phone_number)
-        self.checking_account = checking_account
-        self.savings_account = savings_account
-        
-
-
-
-        
-        
-    #def first_deposit(self,cash_deposit,account_bal):
-
-        #account_bal = account_bal+cash_deposit
-            
-        
-
-        
-
-
-            
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
+        pass
     
-    print("\n****************WELCOME TO PEOPLE'S BANK***********************\n")
-    first_name = input('what is your first name: ')
-    last_name =input('what is last name: ')
-    address = input('Plsese enter tour address: ')
-    phone_number = int(input('Please enter your 10 digit phone number: '))
-    bank_obj = People(first_name,last_name,address,phone_number)
-    bank_obj.data_rec_json(first_name,last_name,address,phone_number)
-    depo_or_wid = int(input('Press 1 for deposit \n Press 2 for withdrawal :'))
-    
-    if depo_or_wid == 1:
-        cash_deposit = int(input('Please enter the amount you want to deposit'))
-        bank_obj.first_deposit(cash_deposit,account_bal=0)
